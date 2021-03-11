149ff56fa1c1d4aff4d6392b02d2c034591a1362
promzzy <promisestephen29@gmail.com>
2021-03-11 22:00:07 +0100

    did mobile responsive on the home page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import PageLayout from './users/components/layouts/PageLayout'
 import HomePage from './routes/userRoute/Index'
 import CreateOrder from './routes/userRoute/order/CreateOrder'
@@ -6,45 +6,20 @@ import CoffeeDetails from './routes/userRoute/coffee/CoffeeDetails'
 import Services from './routes/userRoute/services/Services'
 import About from './routes/userRoute/about/About'
 import ContactUs from './routes/userRoute/contactUs/ContactUs'
-
-//ADMIN IMPORTS
-import AdminLayout from './admin/adminLayout/AdminLayout'
-import AdminHomePage from './routes/admin/index'
-
 function App() {
 
-const AppRoute = ({component:Component, layout:Layout, ...rest})=>(
-  <Route {...rest} render={props=>(<Layout><Component {...props} ></Component></Layout>)}></Route>
-)
+
 
   return (
-    <Router>
-    
+    <Router> 
+      <PageLayout> 
+        <Switch>
 
-      <AppRoute exact path="/" component={HomePage} layout={PageLayout} /> 
+        <Route>
+          <HomePage exact path="/" />
+        </Route>
 
-      <AppRoute exact path="/about/aboutus" component={About} layout={PageLayout} />
-
-      <AppRoute exact path="/services/services" component={Services} layout={PageLayout} />
-
-      <AppRoute exact path="/contact/contactUs" component={ContactUs} layout={PageLayout} />
-
-      <AppRoute exact path="/coffee/details/:id" component={CoffeeDetails} layout={PageLayout} />
-
-      <AppRoute  exact path={["/order/createOrder", "/order/createOrder/:id"]} component={CreateOrder} layout={PageLayout} />
-
-
-
-{/* Addmin Routes */}
-
-    <AppRoute exact path="/admin" component={AdminHomePage} layout={AdminLayout} />
-
-
-      {/* <Route exact path="/">
-        <HomePage />
-      </Route> */}
-      
-      {/* <Route exact path={["/order/createOrder", "/order/createOrder/:id"]}>
+      <Route exact path={["/order/createOrder", "/order/createOrder/:id"]}>
         <CreateOrder />
       </Route>
 
@@ -62,10 +37,11 @@ const AppRoute = ({component:Component, layout:Layout, ...rest})=>(
 
 <Route exact path="/contact/contactUs">
   <ContactUs />
-</Route> */}
-
-      
+</Route>
+</Switch>
+</PageLayout>
     </Router>
+
 
   );
 }

10b2ff6725e8e0f735a148d406348266a0712b46
promzzy <promisestephen29@gmail.com>
2021-03-19 20:41:09 +0100

    edited the footer

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -2,34 +2,50 @@ import React, {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom'
 import styles from './styles/Footer.module.css'
 import {Nav} from './NavData/Nav'
+import { Contacts } from './NavData/Contacts'
+import {coffee} from '../../data/coffees'
 export default function Footer(){
 
 	const [ navs, setNavs ] = useState([]);
+	const [contact, setContact] = useState([])
+	const [coffesProducts, setCoffeeProducts] = useState([])
+
+	const contactusClassN = contact.filter(filteredClassN => (filteredClassN.classN === `contactUs`))
 
 	useEffect(() => {
 		setNavs(Nav)
+		setContact(Contacts)
+		setCoffeeProducts(coffee)
 	},[])
 
     return(
         <>
         <footer>
-	<div className={styles.container}>
+			
 		<div className={styles.footerMenu}>
-			<div>
-				<span class={styles.footerHeadings}>quick link</span>
-				<ul>
+			<div className={styles.footerItems}>
+				<div className={styles.footerHeadings}>quick link</div>
+				<ul className={styles.footerNav}>
 				{
                         navs.map((nav) => (
-                            <Link to={nav.url}>
-                            <li>{nav.title}</li>
+							<li>
+                            <Link className={styles[`${nav.classN}`]} to={nav.url}>
+                            {nav.title}
                             </Link> 
+							</li>
                         ))
                     }
 				</ul>
 			</div>
 			<div>
-				<h5 class={styles.footerHeadings}>location</h5>
-				<p>NO.35 BANEX PLAZA WUSE 2, ABUJA.</p>
+				<h5 class={styles.footerHeadings}>contacts</h5>
+				<ul>
+					{
+						contactusClassN.map((contactus) =>(
+							<li className={styles[contactus.classN]}>{contactus.title}</li>
+						))
+					}
+				</ul>
 			</div>
 			<div>
 				<h5 class={styles.footerHeadings}>SERVICES</h5>
@@ -42,10 +58,15 @@ export default function Footer(){
 			<div>
 				<h5 class={styles.footerHeadings}>PRODUCTION CHAIN</h5>
 				<ul>
-					<li>MOBI-CAFE</li>
-					<li>COFFE BLACK</li>
-					<li>COFFE WHITE</li>
-					<li>COFFE CREAM</li>
+					{
+						coffesProducts.map(products => (
+							<li>
+								<Link className={styles.product} to={`/coffee/details/${products.id}`}>
+								{products.name}
+								</Link>
+							</li>
+						))
+					}
 				</ul>
 			</div>
 			<div>
@@ -63,6 +84,7 @@ export default function Footer(){
 				</div>
 			</div>
 		</div>
+
 		<div>
 			<form>
 				<h3 className={styles.footerHeadings}>SUBSCIBE TO GET UPDATE ON COFFEE INNOVATIONS</h3>
@@ -78,7 +100,7 @@ export default function Footer(){
 
 			</form>
 		</div>
-	</div>
+	
 </footer>
         </>
     )
---
+++ src/components/shared/NavData/Contacts.js
@@ -0,0 +1,32 @@
+
+export const Contacts = [
+    {
+        title: "NO.35 BANEX PLAZA WUSE 2, ABUJA",
+        icon: "",
+        classN: "contactUs"
+    },
+    {
+        title: "Promzzy2020@gmail.com",
+        icon: "",
+        url: "",
+        classN: "contactUs"
+    },
+    {
+        title: "08064453389",
+        icon: "",
+        url: "",
+        classN: "contactUs"
+    },
+    {
+        faceBook: "face book",
+        icon: "",
+        url: "",
+        classN: "socialMedia"
+    },
+    {
+        twitter: "twitter",
+        icon: "",
+        url: "",
+        classN: "socialMedia"
+    }
+]
